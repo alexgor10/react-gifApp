1e1e27f7dbb378c729b5d791b92a746dd2bfe20f
alexgor10 <32745067+alexgor10@users.noreply.github.com>
2020-10-06 21:34:02 -0500

    docs agregado

---
+++ docs/precache-manifest.9069dcb08f4aec3409d83b2af97a5f51.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba85cf265a05808d65ddae735f30308e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2aa8609c40c0f101ce68",
+    "url": "/static/css/main.58fcc149.chunk.css"
+  },
+  {
+    "revision": "1094a9d0787669c6640b",
+    "url": "/static/js/2.ba604ae7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ba604ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2aa8609c40c0f101ce68",
+    "url": "/static/js/main.88c41a01.chunk.js"
+  },
+  {
+    "revision": "daee87f482498cdf724c",
+    "url": "/static/js/runtime-main.29242b1d.js"
+  }
+]);
